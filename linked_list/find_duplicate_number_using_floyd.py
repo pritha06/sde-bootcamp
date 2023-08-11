9c32460696d2bbb53cd8327e9bd0311716451ecd
Pritha <pritha0609@gmail.com>
2023-08-11 10:37:27 +0530

    find duplicate using floyds

---
+++ linked_list/find_duplicate_number_using_floyd.py
@@ -0,0 +1,17 @@
+def find_duplicates(nums):
+    slow,fast=0,0
+    while True:
+        slow=nums[slow]
+        fast=nums[nums[fast]]
+        if slow==fast:
+            break
+    slow2=0
+    while True:
+        slow=nums[slow]
+        slow2=nums[slow2]
+        if slow==slow2:
+            return slow
+
+
+nums=[1,3,4,2,2]
+print(find_duplicates(nums))
